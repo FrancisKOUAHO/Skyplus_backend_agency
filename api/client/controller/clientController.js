f6da9aabea434c7b359a4a9f7400d9e201f61c77
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-05 15:05:45 +0200

    backend ok

--- api/client/controller/clientController.js
+++ api/client/controller/clientController.js
@@ -1,22 +1,68 @@
 const Client = require("../model/Client");
 
+// creer un client
 exports.createClient = (req, res, next) => {
     Client.create(req.body, (error, data) => {
         if (error) {
             return next(error)
         } else {
             res.json(data)
-            console.log('Client successfully created !')
+            console.log('client créé avec succès !')
         }
     })
 };
 
-exports.client =  (req, res) => {
+
+// Recuperer un client
+exports.client = (req, res) => {
     Client.find((error, data) => {
         if (error) {
             return next(error)
         } else {
             res.json(data)
+            console.log('client a été recuperé avec succès !')
         }
     })
 };
+
+// Modifier un client
+exports.editClient = (req, res) => {
+    Client.findById(req.params.id, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+            console.log('client a été modifié avec succès !')
+        }
+    })
+};
+
+// Mise a jour Client
+exports.updateClient = (req, res) => {
+    Client.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data)
+            console.log('client a été mis à jour avec succès !')
+        }
+    })
+};
+
+
+// Supprimer un client
+exports.deleteClient = (req, res) => {
+    Client.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+            console.log('client a été supprimer avec succès !')
+        }
+    })
+};
+
--- api/client/route/client.js
+++ api/client/route/client.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const clientController = require("../controller/clientController");
 
 router.post("/create-client", clientController.createClient);
+router.post("/update-client/:id", clientController.updateClient);
+
 router.get("/get-client", clientController.client);
+router.get("/edit-client/:id", clientController.editClient);
+
+router.delete("/delete-client/:id", clientController.deleteClient);
 
 module.exports = router;
 
