a3d06db57eb9cb23947f26520be5012599c53e4b
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-08 01:20:17 +0200

    stripe ok

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 3001;
-const morgan = require("morgan");
-const cors = require("cors");
+const morgan = require('morgan');
+const cors = require('cors');
 methodOverride = require('method-override');
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 //configure database and mongoose
-mongoose.set("useCreateIndex", true);
+mongoose.set('useCreateIndex', true);
 mongoose
   .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("La base de données est connectéee");
+    console.log('La base de données est connectéee');
   })
-  .catch(err => {
+  .catch((err) => {
     console.log({ database_error: err });
   });
 
@@ -32,17 +32,20 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 //configure body-parser ends here
 
-app.use(morgan("dev")); // configire morgan
+app.use(morgan('dev')); // configire morgan
 
 // define first route
-app.get("/", (req, res) => {
-  res.send("Bonjour, les Skuplusien");
+app.get('/', (req, res) => {
+  res.send('Bonjour, les Skuplusien');
 });
 
-const userRoutes = require("./api/user/route/user"); //bring in our user routes
-const clientRoutes = require("./api/client/route/client"); //bring in our user routes
-app.use("/user", userRoutes);
-app.use("/client", clientRoutes);
+const userRoutes = require('./api/user/route/user'); //bring in our user routes
+const clientRoutes = require('./api/client/route/client'); //bring in our user routes
+const paymentRoutes = require('./api/payments/route/payment'); //bring in our payments routes
+
+app.use('/user', userRoutes);
+app.use('/client', clientRoutes);
+app.use('/payments', paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`L'application fonctionne sur le port ${PORT}`);
