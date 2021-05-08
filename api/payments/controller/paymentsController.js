2a6d20989896118e3d9251a38fcba831dea4e45b
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-08 04:01:43 +0200

    stripe ok

--- api/payments/controller/paymentsController.js
+++ api/payments/controller/paymentsController.js
@@ -1,37 +1,37 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.createCheckoutSession = async (req, res) => {
-  const { priceId } = req.body;
+    const {priceId} = req.body;
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      mode: 'subscription',
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price: priceId,
-          // For metered billing, do not pass quantity
-          quantity: 1,
-        },
-      ],
-      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
-      // the actual Session ID is returned in the query parameter when your customer
-      // is redirected to the success page.
-      success_url:
-        'https://agencyskyplus.herokuapp.com/paiement-reussi/{CHECKOUT_SESSION_ID}',
-      cancel_url: 'https://agencyskyplus.herokuapp.com/echec-paiement',
-    });
+    try {
+        const session = await stripe.checkout.sessions.create({
+            mode: 'subscription',
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price: priceId,
+                    // For metered billing, do not pass quantity
+                    quantity: 1,
+                },
+            ],
+            // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+            // the actual Session ID is returned in the query parameter when your customer
+            // is redirected to the success page.
+            success_url:
+                'https://skyplus-6wnxwaj13-naori-uchiwa.vercel.app/paiement-reussi/{CHECKOUT_SESSION_ID}',
+            cancel_url: 'https://skyplus-6wnxwaj13-naori-uchiwa.vercel.app/echec-paiement',
+        });
 
-    res.send({
-      sessionId: session.id,
-    });
-  } catch (e) {
-    console.log(e.message);
-    res.status(400);
-    return res.send({
-      error: {
-        message: e.message,
-      },
-    });
-  }
+        res.send({
+            sessionId: session.id,
+        });
+    } catch (e) {
+        console.log(e.message);
+        res.status(400);
+        return res.send({
+            error: {
+                message: e.message,
+            },
+        });
+    }
 };
