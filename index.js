0c829b85768032b4b2fb6eb071a77ea499c5012a
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-14 02:50:39 +0200

    stripe ok

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ mongoose
 
 let stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 //registering cors
-/*let corsOptions = {
-    origin: 'https://skyplus.fr',
-    optionsSuccessStatus: 200
-}*/
 app.use(cors());
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
